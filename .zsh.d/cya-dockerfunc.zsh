87f7752ca69394093ffe458fc679539ab94bdb55
Stewart V. Wright <stewart@vifortech.com>
2016-12-02 16:00:54 +1100

    Initial commit of my CYA docker functions

---
+++ .zsh.d/cya-dockerfunc.zsh
@@ -0,0 +1,42 @@
+#!/bin/zsh
+#
+# Wrapper functions for CYA calls to Docker
+# See  https://github.com/v4tech/cya-project
+set -e
+
+CYA_CONTAINER=v4tech/cya-email
+CYA_CONFIG_DIR=${HOME}/work/CYA-config
+
+OFFLINEIMAPCONF=${CYA_CONFIG_DIR}/offlineimap.conf
+SENDPYRC=${CYA_CONFIG_DIR}/sendpyrc
+MSMTPQ_QUEUE=${CYA_CONFIG_DIR}/msmtpq-queue
+
+
+msmtpq() {
+    mkdir -p ${MSMTPQ_QUEUE} && \
+    docker run --rm -i \
+        -v ${SENDPYRC}:/home/mymail/.sendpyrc:ro \
+        -v ${MSMTPQ_QUEUE}:/home/mymail/.config/msmtpq/queue \
+        --name cya-msmtpq \
+        ${CYA_CONTAINER} \
+        msmtpq "$@"
+}
+
+
+msmtp() {
+    docker run --rm -i \
+        -v ${SENDPYRC}:/home/mymail/.sendpyrc:ro \
+        --name cya-msmtp \
+        ${CYA_CONTAINER} \
+        msmtp "$@"
+}
+
+
+offlineimap() {
+    docker run --rm -i \
+        -v ${OFFLINEIMAPCONF}:/home/mymail/.offlineimaprc:ro \
+        -v $HOME/notmuch-mail:/home/mymail/Mail \
+        --name cya-offlineimap \
+        ${CYA_CONTAINER} \
+        fetch-messages.sh "$@"
+}
